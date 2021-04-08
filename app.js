4cec9e862bed8bfada3d1515186ddd812556d7cc
Phil Lee <prodigy7748g@gmail.com>
2021-04-08 22:46:38 +0800

    add checkbox for tasks

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 var app = new Vue({
   el: "#app",
+  components: {
+    'task': {
+              props: ['task'],
+              template: `
+              <div class="ui segment task"
+                   v-bind:class="task.completed ? 'done' : 'todo' ">
+                <div class="ui grid">
+                  <div class="left floated twelve wide column">
+                    <div class="ui checkbox">
+                      <input type="checkbox" name="task" v-model="task.completed">
+                      <label for="task.completed">{{ task.name }} <span class="description">{{ task.description }}</span></label>
+                    </div>
+                  </div>
+                  <div class="right floated three wide column"></div>
+                </div>
+              </div>
+              `
+            }
+  },
   data: {
     tasks:[
       { id: 1, name: "Task 1", description: "this is task 1", completed: false },
