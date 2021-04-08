599c9dff03e3db93e5a97699132ac16cffc09bec
Phil Lee <prodigy7748g@gmail.com>
2021-04-08 14:59:18 +0800

    add todoTasks and completedTasks

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ var app = new Vue({
       { id: 1, name: "Task 1", description: "this is task 1", completed: false },
       { id: 2, name: "Task 2", description: "this is task 2", completed: true },
       { id: 3, name: "Task 3", description: "this is task 3", completed: false }
-    ],
-    message: "hello world"
-  }
+    ]
+  },
+  computed: {
+    completedTasks: function() {
+      return this.tasks.filter( item => item.completed == true );
+    },
+    todoTasks: function() {
+      return this.tasks.filter( item => item.completed == false);
+    }
+  },
 })
